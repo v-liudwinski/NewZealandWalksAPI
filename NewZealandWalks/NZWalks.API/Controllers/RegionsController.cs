1a3fc06fc535af27056d2d685403d6419e100b11
Vitalii Liudvynskyi <vitalii.liudvynskyi@nure.ua>
2022-10-20 15:34:33 +0300

    feat: Initial push

---
+++ NewZealandWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext _dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegions()
+        {
+            var regions = await _dbContext.Regions.ToListAsync();
+            return Ok(regions);
+        }
+    }
+}
